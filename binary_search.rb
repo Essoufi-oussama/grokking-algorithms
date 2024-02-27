353759cc97d96b36b9a9f02122c8d652df4b8455
Essoufi Oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-27 23:57:09 +0100

    did binary search created selection sort

--- binary_search.rb
+++ binary_search.rb
@@ -1,18 +1,18 @@
 def binary_search(list, item)
   low = 0
   high = list.size
-
   while low <= high
     middle = (low + high) / 2
     guess = list[middle]
     if guess == item
       return guess
-    elsif guess > item
+    elsif item < guess
       high = middle - 1
     else
       low = middle + 1
     end
   end
+  return "not found"
 end
 
 my_list = ["a", "b", "c", "d", "e"]
